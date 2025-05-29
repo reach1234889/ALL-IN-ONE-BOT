d2247848a82699e7082a31dc573f28e21c7ee060
reach1234889 <reachreaven4@gmail.com>
2025-05-29 15:47:16 +0800

    Update SetupBanners.js

--- UI/banners/SetupBanners.js
+++ UI/banners/SetupBanners.js
@@ -1,5 +1,5 @@
 const setupBanners = {
-    helpBanner : 'https://i.ibb.co/23C0zG31/standard-1.gif',
+    helpBanner : 'https://i.ibb.co/601Lgh8s/standard.gif',
     verificationBanner : 'https://i.ibb.co/pjHDXzXx/static.png',
     ticketBanner : 'https://i.ibb.co/h1DBsRnv/ticket.gif',
     autovcBanner : 'https://i.ibb.co/mk6Tj1r/autovc.gif',
